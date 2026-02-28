0316efee2905a1f7af1f4c35d201e12c5dc238cc
danon <ipz232_vdb@student.ztu.edu.ua>
2026-02-28 20:06:25 +0200

    Storybook v2

--- src/components/WordGrid.stories.jsx
+++ src/components/WordGrid.stories.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import WordGrid from './WordGrid';
 
 const mockGrid = [
@@ -13,6 +14,9 @@ export default {
   component: WordGrid,
   tags: ['autodocs'],
   argTypes: {
+    cellColor: { control: 'color', description: 'Колір фону звичайної клітинки' },
+    selectedColor: { control: 'color', description: 'Колір фону виділеної клітинки' },
+    foundColor: { control: 'color', description: 'Колір фону знайденої клітинки' },
     onCellMouseDown: { action: 'mousedown' },
     onCellMouseEnter: { action: 'mouseenter' },
     onCellMouseUp: { action: 'mouseup' },
@@ -26,18 +30,26 @@ Empty.args = {
   grid: mockGrid,
   isCellSelected: () => false,
   isCellFound: () => false,
+  cellColor: '#ffffff',
+  selectedColor: '#e0f2fe',
+  foundColor: '#bbf7d0',
 };
 
 export const WithSelected = Template.bind({});
 WithSelected.args = {
-  grid: mockGrid,
+  ...Empty.args,
   isCellSelected: (r, c) => r === 0 && c <= 2,
-  isCellFound: () => false,
+  selectedColor: '#fef08a', 
 };
 
 export const WithFound = Template.bind({});
 WithFound.args = {
-  grid: mockGrid,
-  isCellSelected: () => false,
+  ...Empty.args,
   isCellFound: (r, c) => r === 3,
+  foundColor: '#fca5a5', 
+};
+
+export const Interactive = Template.bind({});
+Interactive.args = {
+  ...Empty.args,
 };
